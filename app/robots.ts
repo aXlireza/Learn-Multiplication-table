94f24c0e719ee0aa97b0cb4ae91c38d827246e23
aXlireza <alireza.ataee01@gmail.com>
2025-04-05 17:21:29 +0330

    Adding Robots and Sitemap

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://mult-tutor.theenigmax.com/sitemap.xml',
+  }
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: '/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
